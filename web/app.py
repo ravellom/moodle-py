d8e960ff3e2ed6487d8ac03434c0423a1d777c7a
Raidell Avello <ravellom@gmail.com>
2021-06-10 10:00:05 -0400

    General update

--- web/app.py
+++ web/app.py
@@ -3,9 +3,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from my_utils import descom_fecha
-from make_plots import sns_plot_user
-
+from my_utils import load_data, descom_fecha
+from make_plots import sns_plot_user1, sns_plot_user2
+menu_types = (
+    "Cursos",
+    "Usuarios",
+    "Actividades",
+) 
 st.write("""
 # Análisis de reportes de Moodle
 
@@ -18,53 +22,44 @@ st.sidebar.header('Datos de entrada')
 # @st.cache
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 
-
 # Chequear si se subió un fichero
 if uploaded_file is not None:
+    # Load data
+    df = load_data(uploaded_file)
 
     # Construir cuerpo de la app para fichero subido
-    df = pd.read_csv(uploaded_file)
-    df = df.rename(columns={
-                    'Hora': 'DT',
-                    'Nombre completo del usuario': 'Name',
-                    'Usuario afectado': 'User_afec',
-                    'Contexto del evento': 'Context',
-                    'Componente': 'Component',
-                    'Nombre evento': 'Event',
-                    'Descripción': 'Description',
-                    'Origen': 'Origen',
-                    'Dirección IP': 'IP',
-                })
-    df = df.loc[df["Name"] != "-"]
-    df = descom_fecha(df)
 
-    # Sidebar
-    n_users = st.sidebar.slider('Usuarios más activos a mostrar', 1, 50, 20)
-    n_context = st.sidebar.slider('Cursos más activos a mostrar', 1, 50, 20)
+    # General sidebar
+    menu = st.sidebar.selectbox("Seleccionar análisis", menu_types)
+    show_table = st.sidebar.checkbox("Ver tabla?", False)
 
-    # Main
-    st.subheader('Registros a analizar')
-    st.write(df)
+    # Main column
+    if show_table: 
+        st.subheader('Registros a analizar')
+        st.write(df)
 
-    st.subheader('Usuarios más activos por cantidad de accesos')
-    fig = sns.catplot(y="Name", kind="count",
-                palette="ch:.25", edgecolor=".6",
-                data=df, order=df['Name'].value_counts().iloc[:n_users].index)
-    st.pyplot(fig)
+    # If is menu General show general stuffs
+    if menu == "Cursos":
 
-    st.subheader('Cursos más activos por cantidad de accesos')
-    fig = sns.catplot(y="Context", kind="count",
-                palette="ch:.25", edgecolor=".6",
-                data=df, order=df['Context'].value_counts().iloc[:n_context].index)
-    st.pyplot(fig)
+        n_context = st.sidebar.slider('Cursos más activos a mostrar', 1, 50, 20)
 
-    
+        # Main column
+        st.subheader('Cursos más activos por cantidad de accesos')
+        fig = sns.catplot(y="Context", kind="count",
+                    palette="ch:.25", edgecolor=".6",
+                    data=df, order=df['Context'].value_counts().iloc[:n_context].index)
+        st.pyplot(fig)
 
-
-
+    if menu == "Usuarios":
+        # Sidebar
+        n_users = st.sidebar.slider('Usuarios más activos a mostrar', 1, 50, 20)
+        # Main column
+        st.subheader('Usuarios más activos por cantidad de accesos')
+        st.pyplot(sns_plot_user1(df, n_users))
+        st.subheader('Usuarios conectados por días')
+        st.pyplot(sns_plot_user2(df))        
 
 else:
-
     st.write("Suba un fichero csv para iniciar.")
 
     
--- web/my_utils.py
+++ web/my_utils.py
@@ -1,10 +1,30 @@
 import pandas as pd
 import datetime
 
+
+def load_data(uploaded_file):
+    df = pd.read_csv(uploaded_file)
+    df = df.rename(columns={
+                    'Hora': 'DT',
+                    'Nombre completo del usuario': 'Name',
+                    'Usuario afectado': 'User_afec',
+                    'Contexto del evento': 'Context',
+                    'Componente': 'Component',
+                    'Nombre evento': 'Event',
+                    'Descripción': 'Description',
+                    'Origen': 'Origen',
+                    'Dirección IP': 'IP',
+                })
+    df = df.loc[df["Name"] != "-"]
+    df = descom_fecha(df)
+    return df
+
+
 def descom_fecha(df):
     # extraer fecha
     df['datefull'] = pd.to_datetime(df['DT'])
     # Date
+    df['date']= df['datefull'].dt.date
     df['year']= df['datefull'].dt.year
     df['month']= df['datefull'].dt.month
     df['day']= df['datefull'].dt.day
